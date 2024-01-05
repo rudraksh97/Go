433987466b82962540e0f533d01c56f2d5f7a51f
Rudraksh Agarwal <rudrakshiitd@outlook.com>
2024-01-06 00:06:44 +0530

    docs: add comments to run

--- hello-world.go
+++ hello-world.go
@@ -5,3 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// How to run?
+// $ go run hello-world.go
+// hello world
+
+// How to build?
+// $ go build hello-world.go
+// $ ls
+// hello-world    hello-world.go
+
+// How to run built file
+// $ ./hello-world
+// hello world
